5024ef599a5d783233a9f0f7b5902e0f15b61105
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-08 01:11:58 +0600

    Create Basic c++.cpp

---
+++ Basic c++.cpp
@@ -0,0 +1,27 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+
+1. Introduction to C++ | Data Structures and Algorithms | College Placement Course | Lecture 1
+
+0:00 - Course Overview
+1:42 - Introduction to C++ programming
+4:09 - Flowchart and Pseudocode
+16:26 - Setting up VS code on Windows
+18:41 - Setting up VS code on Mac
+23:16 - Brain Teaser
+
+VS Code Installation for C++ in Windows | Step by step process explanation
+
+2.1 Data Types & Type Modifiers | Data Structures & Algorithms Course in C++ | Lecture 2.1
+
+2.3 If/else statement in C++ programming | Data Structure and Algorithm Course | Lecture 2.3
+
+2.4 Introduction to Loops in C++ programming | Guaranteed Placement Course | Lecture 2.4
+
+3.1 Break and Continue statement in C++ Programming | Guaranteed Placement Course | Lecture 3.1
+
+3.3 Operators in C++ Programming | Guaranteed Placement Course | 3.3
