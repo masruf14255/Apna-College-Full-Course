aa040ec01917b1a2571382514f61f41742365f6c
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-25 18:41:12 +0600

    Create CountSort.cpp

---
+++ CountSort.cpp
@@ -0,0 +1,51 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+
+*/
+
+Count Sort | C++ Placement Course | Lecture 20.1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void countSort(int arr[], int i)
+{
+    int k=arr[0];
+    for(int i=0; i<n; i++)
+    {
+        k=max(k,arr[i]);
+    }
+    int count[10]={0};
+    for(int i=0; i<n; i++)
+    {
+        count[arr[i]]++;
+    }
+    for(int i=1; i<=k; i++)
+    {
+        count[i]+=count[i-1];
+    }
+    int output[n];
+    for(int i=n-1; i>=0; i--)
+    {
+        output[--count[arr[i]]]==arr[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        arr[i]=output[i];
+    }
+
+}
+
+int main()
+{
+    int arr[]={1,3,2,3,4,1,6,4,3};
+    countSort(arr,9);
+
+    for(int i=0; i<9; i++)
+    {
+        cout<<arr[i]<<|" ";
+    }
+}
